27c28b31c8bc87afd05af126093422c3532234c3
Eugenia Subirons <eugenia.subirons@gmail.com>
2023-05-23 09:36:19 +0200

    okay

---
+++ Metodoss1.py
@@ -0,0 +1,25 @@
+
+def say_hello():
+    print('Hello World')
+
+def say_goodbye(name):
+    print('Goodbye ' + name)
+
+say_hello()
+say_goodbye('Eugi')
+
+def suma_numeros(x, y):
+    suma = x + y
+    return suma
+
+def resta_numeros(x,y):
+    resta = x - y
+    return resta
+
+sumaaa= suma_numeros(4, 8)
+restaaa= resta_numeros(7, 2)
+
+print('La suma es: ', sumaaa)
+print('La resta es: ', restaaa)
+
+
